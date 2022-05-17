833fe0bd0b0503f414b038143521409340d988e4
David Chen <david.chen@alliofintech.io>
2022-05-16 22:23:33 -0700

    deployed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import solid from "@astrojs/solid-js";
+
+console.log(process.env.NODE_ENV);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid()]
+  integrations: [solid()],
+  outDir: './docs',
+  site: 'https://clfhhc.github.io',
+  base: process.env.NODE_ENV === 'production' ? '/try-astro-solid' : '',
 });
